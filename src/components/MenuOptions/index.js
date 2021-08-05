89df7d3dfbdf352a56db41a65bdc00c7b28eefab
oricardos <ricardo.rs95@gmail.com>
2021-08-05 18:30:17 -0300

    feat(slide): Add new features

--- src/components/MenuOptions/index.js
+++ src/components/MenuOptions/index.js
@@ -2,30 +2,21 @@ import React from 'react';
 import {View, Text, TouchableOpacity, StyleSheet} from 'react-native';
 import {AntDesign} from "@expo/vector-icons";
 
+import { Container, Option, TextOption } from "./styles";
+
 export default function MenuOptions({ iconName, text }) {
     return (
         <>
             <TouchableOpacity>
-                <View style={{
-                    flexDirection: 'row',
-                    alignContent: 'center',
-                    justifyContent: 'space-between',
-                    marginVertical: '5%'
-                }}>
-                    <View style={{flexDirection: 'row', alignContent: 'center'}}>
-                        <AntDesign name={iconName} size={20} color="#ff0000"/>
-                        <Text style={{alignSelf: 'center', marginLeft: 15}}>{text}</Text>
-                    </View>
-                    <AntDesign name="right" size={20} color="#ff0000"/>
-                </View>
+                <Container>
+                    <Option>
+                        <AntDesign name={iconName} size={20} color="#757D8A"/>
+                        <TextOption>{text}</TextOption>
+                    </Option>
+                    <AntDesign name="right" size={20} color="#757D8A"/>
+                </Container>
             </TouchableOpacity>
         </>
 
     )
 };
-
-const styles = StyleSheet.create({
-    container: {
-        width: '100%',
-    }
-});
---
+++ src/components/MenuOptions/styles.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+    flex-direction: row;
+    align-content: center;
+    justify-content: space-between;
+    margin: 1rem 0;
+`;
+
+export const Option = styled.View`
+    flex-direction: row;
+    align-content: center;
+`;
+
+export const TextOption = styled.Text`
+    align-self: center;
+    margin-left: 1.25rem;
+`;
